ac75795d4eb2040335ca835ad6f23509aecff6b6
NIKO RIANT CAHYONO <nikoriantcahyono@gmail.com>
2023-12-10 11:30:06 +0700

    update data.js

--- frontend/src/lib/data.js
+++ frontend/src/lib/data.js
@@ -1,6 +1,71 @@
 const dataProduct = [{
-    imageUrl: "",
-    product: "",
-    description: "",
+    imageUrl: "https://filebroker-cdn.lazada.co.id/kf/S09d3cc96533348ce8b166072f75be9d23.jpg",
+    product: "Beras Koi 5KG",
+    description: "Beras Koi 5kg adalah varian beras berkualitas tinggi dengan butiran yang panjang dan aroma yang harum. Cocok untuk kebutuhan memasak sehari-hari.",
     rating: ""
+},{
+    imageUrl: "https://lzd-img-global.slatic.net/g/p/7b2c4f1ef0aaa0a93659a1b51ac7e739.jpg_720x720q80.jpg",
+    product: "HappyTos",
+    description: "Happy Tos adalah merek biskuit yang terkenal dengan rasa yang lezat dan tekstur yang renyah. Cocok sebagai camilan ringan untuk keluarga.",
+    rating: ""  
+},{
+    imageUrl: "https://assets.klikindomaret.com/products/10031595/10031595_1.jpg",
+    product: "Roma Kelapa",
+    description: " Biskuit Roma kelapa memiliki rasa kelapa yang autentik dan kaya, memberikan pengalaman menyenangkan dalam setiap gigitannya. Cocok untuk dinikmati bersama teh atau kopi.",
+    rating: ""  
+},{
+    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVlyWaXPgdIHbd2frJ9n3YirJhEr8j4DxYgQ&usqp=CAU",
+    product: "Rinso Matic 1.65L",
+    description: "Rinso Matic 2L adalah deterjen cair yang dirancang khusus untuk mesin cuci otomatis. Membersihkan pakaian secara efektif dan memberikan keharuman yang tahan lama.",
+    rating: ""  
+},{
+    imageUrl: "https://images.tokopedia.net/img/cache/700/VqbcmM/2021/1/15/2f06e8b7-01c9-43d9-b8ce-e507d34329c2.png",
+    product: "Daia 2KG",
+    description: "Daia 2kg adalah merek deterjen bubuk dengan formula yang efektif menghilangkan noda pada pakaian. Memberikan kebersihan optimal pada setiap cucian.",
+    rating: ""  
+},{
+    imageUrl: "https://images.tokopedia.net/img/cache/500-square/VqbcmM/2022/1/21/bb1c444e-b09b-45e9-9dad-4bf92b34f348.jpg",
+    product: "LifeBuoy cair 450ml",
+    description: "",
+    rating: ""  
+},{
+    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWeD-kSUUF0h_jiqaUlt2BzSTFxe053xvoy91VPKt5dw&s",
+    product: "Sunlight 755Ml",
+    description: "Sunlight adalah sabun cuci piring yang terkenal dengan keampuhannya menghilangkan lemak dan kotoran pada peralatan dapur. Memberikan hasil bersih dan kilau.",
+    rating: ""  
+},{
+    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7qcYcwGgDeaUeUZnzDwvtbfKrUsoLSv6YFg&usqp=CAU",
+    product: "Nuvo Cair 450ml",
+    description: "Nuvo adalah merek sabun mandi yang menyegarkan dan membersihkan kulit dengan lembut. Tersedia dalam berbagai varian aroma untuk memberikan pengalaman mandi yang menyenangkan.",
+    rating: ""  
+},{
+    imageUrl: "https://filebroker-cdn.lazada.co.id/kf/S78c3e82b3ed94ccaa21ac871a225acaei.jpg",
+    product: "Beras Bintang 5kg",
+    description: "Beras Bintang 5kg adalah pilihan beras premium dengan kualitas unggul, memberikan hasil nasi yang lembut dan pulen. Cocok untuk acara khusus dan masakan istimewa.",
+    rating: ""  
+},{
+    imageUrl: "https://www.masakapahariini.com/wp-content/uploads/2018/04/Bango-Kecap-Manis-1.png",
+    product: "Kecap Bango",
+    description: "Kecap Bango adalah kecap manis yang terkenal dengan cita rasa khasnya. Cocok sebagai bumbu penyedap pada berbagai masakan Indonesia, memberikan sentuhan manis yang lezat.",
+    rating: ""  
+},{
+    imageUrl: "https://id-test-11.slatic.net/p/67bbf7719b9b3688427af801ab497a9c.jpg",
+    product: "Minyak Sunco 2L",
+    description: "Minyak Goreng Sunco 2L adalah pilihan minyak goreng berkualitas tinggi. Cocok untuk menggoreng berbagai jenis masakan dengan hasil yang lezat dan garing.",    
+    rating: ""  
+},{
+    imageUrl: "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//102/MTA-3744948/tropical_tropical-minyak-goreng-pet-2l_full02.jpg",
+    product: "minyak Goreng Tropical 2L",
+    description: "Minyak Goreng Tropical 2L adalah pilihan minyak goreng berkualitas tinggi. Cocok untuk menggoreng berbagai jenis masakan dengan hasil yang lezat dan garing.",
+    rating: ""  
+},{
+    imageUrl: "https://assets.klikindomaret.com/products/10003517/10003517_1.jpg",
+    product: "Indomie goreng 80g",
+    description: "Indomie Goreng dan Indomie Soto adalah varian mie instan yang populer. Indomie Goreng memiliki rasa gurih dan pedas, sementara Indomie Soto memberikan cita rasa kuah khas soto. Cocok untuk sajian cepat dan praktis.",
+    rating: ""  
+},{
+    imageUrl: "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//89/MTA-17813203/indomie_indomie_kuah_rasa_soto_mie_70g_-_star_farm_full02_ejj3qfh9.jpg",
+    product: "Indomie kuah Soto 80g",
+    description: "Indomie Goreng dan Indomie Soto adalah varian mie instan yang populer. Indomie Goreng memiliki rasa gurih dan pedas, sementara Indomie Soto memberikan cita rasa kuah khas soto. Cocok untuk sajian cepat dan praktis.",
+    rating: ""  
 }]
